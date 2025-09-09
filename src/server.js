6996f3558600e5ac7f6d5b8fddd16381ace33d26
KenzoSebastian <kenzosebastian4@gmail.com>
2025-09-09 14:39:32 +0700

    utils: removed cron, deploy api to vercel

--- src/server.js
+++ src/server.js
@@ -1,18 +1,18 @@
 import express from "express";
 import "dotenv/config";
 import { ENV } from "./config/env.js";
-import job from "./config/cron.js";
 
 const app = express();
 const PORT = ENV.PORT || 3000;
-
-if (ENV.NODE_ENV === "production") job.start();
 
 // Middleware to parse JSON requests
 app.use(express.json());
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ status: "OK", message: "Server is healthy" });
+});
+app.get("/api/testing", (req, res) => {
+  res.status(200).json({ status: "OK", message: "ini ada testing route" });
 });
 
 app.listen(PORT, () => {
