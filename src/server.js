f476b4ff46f43b4bbaa86a0a471b01380a6b6abd
KenzoSebastian <kenzosebastian4@gmail.com>
2025-09-09 14:16:17 +0700

    utils: cron.js added

--- src/server.js
+++ src/server.js
@@ -1,14 +1,20 @@
 import express from "express";
 import "dotenv/config";
 import { ENV } from "./config/env.js";
+import job from "./config/cron.js";
 
 const app = express();
+const PORT = ENV.PORT || 3000;
+
+if (ENV.NODE_ENV === "production") job.start();
+
+// Middleware to parse JSON requests
+app.use(express.json());
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ status: "OK", message: "Server is healthy" });
 });
 
-const PORT = ENV.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
