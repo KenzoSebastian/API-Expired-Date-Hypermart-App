498657006433aea7157883b3a97a6cb48ee6cc9d
KenzoSebastian <kenzosebastian4@gmail.com>
2025-11-10 16:20:18 +0700

    fix: axios instance

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { ENV } from "./env.js";
 
 export const axiosInstance = axios.create({
-  baseURL: `${ENV.BASE_URL}${ENV.PORT}/api`,
+  baseURL: `${ENV.BASE_URL}/api`,
   headers: {
     "Content-Type": "application/json",
   },
